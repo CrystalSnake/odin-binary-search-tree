807eafd0ffc3b2bb046c8d6530d8102d9b7c6b4e
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-05-09 17:42:07 +0400

    Change driver script

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 import Tree from './tree.js';
 import prettyPrint from './pretty.js';
 
-const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-
-const tree = new Tree(testArr);
-//prettyPrint(tree.root);
-
-//console.log(tree);
-// console.log(tree.find(4));
-// console.log(tree.find(3));
-// console.log(tree.find(67));
-// console.log(tree.find(667));
-tree.insert(14);
-tree.insert(25);
-//prettyPrint(tree.root);
-
-//tree.delete(67);
-prettyPrint(tree.root);
-console.log(`Tree height is ${tree.height()}.`);
-console.log(`Level order ${tree.levelOrder()}.`);
-
 function double(n) {
   return n * 2;
 }
 
-console.log(`Level order ${tree.levelOrder(double)}.`);
-console.log(`Inorder ${tree.inorder()}.`);
-console.log(`Inorder ${tree.inorder(double)}.`);
-console.log(`Preorder ${tree.preorder()}`);
-console.log(`Preorder ${tree.preorder(double)}`);
-console.log(`Postorder ${tree.postorder()}`);
-console.log(`Postorder ${tree.postorder(double)}`);
-console.log(tree.depth(tree.find(4)));
-console.log(tree.isBalanced());
-tree.insert(11);
-tree.insert(22);
-console.log(tree.isBalanced());
-tree.rebalance();
-console.log(tree.isBalanced());
+function getRandomArray(n, limit) {
+  return Array(n)
+    .fill()
+    .map(() => Math.round(Math.random() * limit));
+}
+
+const randomTree = new Tree(getRandomArray(50, 10000));
+prettyPrint(randomTree.root);
+console.log(randomTree.isBalanced());
+console.log(`Level order ${randomTree.levelOrder()}.`);
+console.log(`Inorder ${randomTree.inorder()}.`);
+console.log(`Preorder ${randomTree.preorder()}`);
+console.log(`Postorder ${randomTree.postorder()}`);
+
+getRandomArray(100, 1000).forEach((element) => randomTree.insert(element));
+console.log(randomTree.isBalanced());
+randomTree.rebalance();
+console.log(randomTree.isBalanced());
+console.log(`Level order ${randomTree.levelOrder()}.`);
+console.log(`Inorder ${randomTree.inorder()}.`);
+console.log(`Preorder ${randomTree.preorder()}`);
+console.log(`Postorder ${randomTree.postorder()}`);
+prettyPrint(randomTree.root);
